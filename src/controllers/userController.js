4649f3c1fbd6229a6829eddc5a7c502cc9a974b8
djdu4496 <djdu4496@gmail.com>
2025-02-07 19:37:54 +0900

    6.3 Form Validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,8 +2,16 @@ import User from "../models/User.js";
 
 export const getJoin = (req, res) => res.render("join", {pageTitle: 'Join'});
 export const postJoin = async (req, res) => {
-  console.log(req.body);
-  const {name, email, username, password, location} = req.body;
+  const {name, email, username, password, password2, location} = req.body;
+  const exists = await User.exists({$or: [{username}, {email}]});
+  const pageTitle = "Join";
+  if(password !== password2) {
+    return res.render("join",  {pageTitle, errorMessage: "Password confirmation doesn't match."});
+  }
+  if(exists) {
+    return res.render("join",  {pageTitle, errorMessage: "This username/email is already taken."});
+  }
+
   await User.create({
     name, email, username, password, location
   })
