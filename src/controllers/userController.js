3baf52d754ea63aba3768e128e74a3eaab20a2f7
djdu4496 <djdu4496@gmail.com>
2025-02-11 23:45:26 +0900

    6.6 Login part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", {pageTitle: 'Join'});
 export const postJoin = async (req, res) => {
@@ -11,13 +12,35 @@ export const postJoin = async (req, res) => {
   if(exists) {
     return res.status(404).render("join",  {pageTitle, errorMessage: "This username/email is already taken."});
   }
-
-  await User.create({
-    name, email, username, password, location
-  })
-  res.redirect("/login");
+  try {
+    await User.create({
+      name, email, username, password, location
+    })
+    res.redirect("/login");
+  } catch (error) {
+    return res.status(404).render("join", { pageTitle, errorMessage: error._message})
+  }
 };
-export const login = (req, res) => res.send("Login");
+export const getLogin = (req, res) => {
+  return res.render("login", {pageTitle: "Login", errorMessage: ""})
+}
+export const postLogin = async (req, res) => {
+  // check if account exists
+  // check if password correct
+  const {username, password} = req.body;
+  const pageTitle = "Login";
+  const user = await User.findOne({username});
+  if(!user){
+    return res.status(400).render("login", {pageTitle, errorMessage: "An account with this username does not exists."})
+  }
+  const match = await bcrypt.compare(password, user.password);
+
+  if(!match) {
+    return res.status(400).render("login", {pageTitle, errorMessage: "Wrong password."})
+  }
+  console.log("Log User in! Coming Soon!")
+  return res.redirect("/");
+}
 export const edit = (req, res) => res.send("Edit User.");
 export const remove = (req, res) => res.send("Delete User.");
 export const logout = (req, res) => res.send("Log out.");
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { getJoin, postJoin, login } from "../controllers/userController.js";
+import { getJoin, postJoin, getLogin, postLogin } from "../controllers/userController.js";
 import { home, search } from "../controllers/videoController.js";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.get("/login", login);
+rootRouter.route("/login").get(getLogin).post(postLogin);
 rootRouter.route("/join").get(getJoin).post(postJoin);
 rootRouter.get("/search", search);
 export default rootRouter;
